a9207f57c78d7c2d2b8aa15386cfaffaec2e6093
Martin Nilsson <martin.nordkvist@zoho.com>
2017-04-06 11:15:17 +0200

    DataAccess class added

---
+++ Restaurantservice/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantservice
+{
+    public class DataAccess
+    {
+        public static void GetTentativeOrders()
+        {
+
+
+
+        }
+
+
+
+    }
+}
